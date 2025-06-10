d7ace0a086c34253640de28c02a9022815d37882
Adam Griffin <ag@agriffin.me>
2025-06-10 10:55:28 -0700

    Added data to boundary route response

--- handlers/listing_handler.go
+++ handlers/listing_handler.go
@@ -9,12 +9,57 @@ import (
 )
 
 type ListingQueryResult struct {
-	ID           uint            `json:"id"`
-	Line1        string          `json:"line1"`
-	Geometry     json.RawMessage `json:"geometry"`
-	Neighborhood *string         `json:"neighborhood"`
+	ID            int64
+	Slug          string
+	ListPrice     uint
+	ListedDate    string
+	AddressLine_1 string
+	AddressLine_2 string
+	City          string
+	State         string
+	Zip           string
+	Latitude      float64
+	Longitude     float64
+	PlaceId       string
+	Neighborhood  string
+	Status        string
+	Description   string
+	Beds          uint
+	Baths         uint
+	Sqft          uint
+	LotSize       uint
+	View          bool
+	Waterfront    bool
+	PhotoGallery  json.RawMessage
+}
+
+type ListingAddress struct {
+	Line1 string `json:"line1"`
+	Line2 string `json:"line2"`
+	City  string `json:"city"`
+	State string `json:"state"`
+	Zip   string `json:"zip"`
+}
+
+type ListingResponse struct {
+	ID           int64           `json:"id"`
+	Slug         string          `json:"slug"`
+	ListPrice    uint            `json:"listPrice"`
+	ListedDate   string          `json:"listedDate"`
+	Address      ListingAddress  `json:"address"`
+	Latitude     float64         `json:"latitude"`
+	Longitude    float64         `json:"longitude"`
+	PlaceId      string          `jsong:"placeId"`
+	Neighborhood string          `json:"neighborhood"`
+	Status       string          `json:"status"`
+	Description  string          `json:"description"`
+	Beds         uint            `json:"beds"`
+	Baths        uint            `json:"baths"`
+	Sqft         uint            `json:"sqft"`
+	LotSize      uint            `josn:"lotSize"`
 	View         bool            `json:"view"`
 	Waterfront   bool            `json:"waterfront"`
+	PhotoGallery json.RawMessage `json:"photoGallery"`
 }
 
 var booleanFilters = map[string]string{
@@ -26,8 +71,45 @@ func GetListingsInsideBoundary(ctx *gin.Context) {
 	placeID := ctx.Param("place_id")
 
 	query := db.Database.Table("listings l").
-		Select(`l.id, l.line1, ST_AsGeoJSON(l.geom, 15)::json AS "geometry", l.neighborhood, l.view, l.waterfront`).
+		Select(
+			"l.id",
+			"l.slug",
+			"l.list_price",
+			"l.listed_date",
+			"l.address_line_1",
+			"l.address_line_2",
+			"l.city",
+			"l.state",
+			"l.zip",
+			"ST_Y(l.geom) AS latitude",
+			"ST_X(l.geom) AS longitude",
+			"l.place_id",
+			"l.neighborhood",
+			`ls.name AS "status"`,
+			"l.description",
+			"l.beds",
+			"l.baths",
+			"l.sqft",
+			"l.lot_size",
+			"l.view",
+			"l.waterfront",
+			"COALESCE(pg_agg.photo_gallery, '[]') AS photo_gallery",
+		).
 		Joins("JOIN boundaries b ON ST_Contains(b.geom, l.geom)").
+		Joins("LEFT JOIN listing_statuses ls ON l.listing_status_id = ls.id").
+		Joins(`
+			LEFT JOIN LATERAL (
+				SELECT
+					jsonb_agg(
+						jsonb_build_object('url', pgi.url, 'caption', pgi.caption)
+					) AS photo_gallery
+				FROM
+					photo_galleries pg
+					JOIN photo_gallery_images pgi ON pg.id = pgi.photo_gallery_id
+				WHERE
+					pg.listing_id = l.id
+			) AS pg_agg ON TRUE
+		`).
 		Where("b.place_id = ?", placeID)
 
 	for param, column := range booleanFilters {
@@ -37,6 +119,7 @@ func GetListingsInsideBoundary(ctx *gin.Context) {
 	}
 
 	var results []ListingQueryResult
+	var response []ListingResponse
 
 	err := query.Scan(&results).Error
 	if err != nil {
@@ -44,5 +127,34 @@ func GetListingsInsideBoundary(ctx *gin.Context) {
 		return
 	}
 
-	ctx.JSON(http.StatusOK, results)
+	for _, l := range results {
+		r := ListingResponse{
+			ID:           l.ID,
+			Slug:         l.Slug,
+			ListPrice:    l.ListPrice,
+			ListedDate:   l.ListedDate,
+			Latitude:     l.Latitude,
+			Longitude:    l.Longitude,
+			PlaceId:      l.PlaceId,
+			Neighborhood: l.Neighborhood,
+			Status:       l.Status,
+			Description:  l.Description,
+			Beds:         l.Beds,
+			Baths:        l.Baths,
+			Sqft:         l.Sqft,
+			LotSize:      l.LotSize,
+			View:         l.View,
+			Waterfront:   l.Waterfront,
+			PhotoGallery: l.PhotoGallery,
+		}
+		r.Address.Line1 = l.AddressLine_1
+		r.Address.Line2 = l.AddressLine_2
+		r.Address.City = l.City
+		r.Address.State = l.State
+		r.Address.Zip = l.Zip
+
+		response = append(response, r)
+	}
+
+	ctx.JSON(http.StatusOK, response)
 }
